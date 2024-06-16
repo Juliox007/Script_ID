02c8223bc307fd652a4d62cb7f6b6bdc45950292
julio <juliosaki69@gmail.com>
2024-06-16 01:03:33 -0400

    Script_for_personal_ID

---
+++ file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Autor: Jules
+#Date: June 2024
+
+#Prompting client for input.
+echo "Welcome! Please provide us some information."
+read -p "your name:" name
+read -p "your age:" age
+read -p "your city:" city
+read -p "your profession:" profession
+
+# Displaying a message with gathered information
+echo ""
+echo "Hello $name!your age is $age, your city is $city and your profession is $profession"git 
